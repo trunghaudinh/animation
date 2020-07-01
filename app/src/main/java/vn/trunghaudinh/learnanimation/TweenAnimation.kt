e29fa973e83e83f4d9a0a45c89730e74bdbf0b03
TrungHauDinh <trunghaukmhd@gmail.com>
2020-07-01 14:56:17 +0700

    change

--- app/src/main/java/vn/trunghaudinh/learnanimation/TweenAnimation.kt
+++ app/src/main/java/vn/trunghaudinh/learnanimation/TweenAnimation.kt
@@ -1,48 +1,68 @@
 package vn.trunghaudinh.learnanimation
 
-import android.animation.AnimatorSet
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationSet
-import android.view.animation.AnimationUtils
+import android.view.animation.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_tween_animation.*
 
 class TweenAnimation : AppCompatActivity() {
 
-    var animationTranslate : Animation? = null
-    var animationScale : Animation? = null
-    var animationAlpha : Animation? = null
-    var animationRotation : Animation? = null
+    var animationTranslate: Animation? = null
+    var animationScale: Animation? = null
+    var animationAlpha: Animation? = null
+    var animationRotation: Animation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tween_animation)
 
-        animationTranslate = AnimationUtils.loadAnimation(this,R.anim.anim_translate)
-        animationScale = AnimationUtils.loadAnimation(this,R.anim.anim_scale)
-        animationRotation = AnimationUtils.loadAnimation(this,R.anim.anim_rotation)
-        animationAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha)
+        animationTranslate = AnimationUtils.loadAnimation(this, R.anim.anim_translate)
+        animationScale = AnimationUtils.loadAnimation(this, R.anim.anim_scale)
+        animationRotation = AnimationUtils.loadAnimation(this, R.anim.anim_rotation)
+        animationAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha)
 
 
         btnStartTranslate.setOnClickListener {
             val animationSet = AnimationSet(false)
-            animationSet.addAnimation(animationTranslate)
-//            animationSet.addAnimation(animationAlpha)
-            animationSet.addAnimation(animationRotation)
+
+            val rotationAnim = RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF,.5f, Animation.RELATIVE_TO_SELF,.5f)
+            rotationAnim.duration = 10000
+
+            val translateAnim = TranslateAnimation(0f, 0f, 0f, 600f)
+            translateAnim.duration = 10000
+
+
+
+            animationSet.addAnimation(rotationAnim)
+            animationSet.addAnimation(translateAnim)
+
             view1.startAnimation(animationSet)
+
+
+
+
+        }
+        view1.setOnClickListener {
+            Toast.makeText(this@TweenAnimation,"animation cliked",Toast.LENGTH_LONG).show()
         }
 
         btnStartScale.setOnClickListener {
-            view1.startAnimation(animationScale)
+            val obj = ObjectAnimator.ofFloat(view1,"translationY",0f,300f)
+            obj.duration = 3000
+            obj.start()
+
         }
 
-        btnStartRotation.setOnClickListener{
+        btnStartRotation.setOnClickListener {
             view1.startAnimation(animationRotation)
         }
 
-        btnStartAlpha.setOnClickListener{
+        btnStartAlpha.setOnClickListener {
             view1.startAnimation(animationAlpha)
+
         }
 
     }
