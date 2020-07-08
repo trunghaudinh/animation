2a9fe96ae4fb17a26f2aa35c497ed1c38cf056aa
TrungHauDinh <trunghaukmhd@gmail.com>
2020-07-08 18:21:22 +0700

    Update motionlayout

--- app/src/main/java/vn/trunghaudinh/learnanimation/MotionActivity.kt
+++ app/src/main/java/vn/trunghaudinh/learnanimation/MotionActivity.kt
@@ -1,11 +1,21 @@
 package vn.trunghaudinh.learnanimation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MotionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_motion)
+
+//        view.setOnClickListener {
+//            Toast.makeText(this,"Start",Toast.LENGTH_LONG).show()
+//        }
     }
 }
